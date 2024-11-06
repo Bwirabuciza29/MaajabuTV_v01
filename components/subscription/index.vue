eecea7804322f61587b8f6a53f6a12aa930490f8
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-06 21:19:31 +0200

    HereWeAre

--- components/subscription/index.vue
+++ components/subscription/index.vue
@@ -1,49 +1,41 @@
-<!-- pages/subscription.vue -->
+
 <template>
   <div
-    class="flex items-center justify-center min-h-screen bg-pattern bg-cover bg-center p-4"
+    class="flex items-center justify-center bg-pattern bg-cover bg-center p-4"
   >
-    <div
-      class="bg-black bg-opacity-80 text-center text-white p-8 rounded-lg max-w-md w-full"
-    >
-      <h1 class="text-2xl font-bold mb-4">
+    <div class="text-center text-yellow-300 p-8 rounded-lg max-w-md w-full">
+      <h1 class="text-3xl font-bold mb-4">
         Nouvel épisode<br />chaque semaine
       </h1>
       <p class="text-gray-300 mb-6">
         Les dernières mises à jour directement dans votre boîte de réception.
       </p>
-      <form @submit.prevent="handleSubscribe" class="flex flex-col space-y-4">
+      <div class="relative max-w-lg mx-auto">
         <input
-          type="email"
-          placeholder="Add your email here"
-          v-model="email"
-          class="px-4 py-2 rounded bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-yellow-500"
-          required
+          type="text"
+          class="w-full bg-black py-4 pl-3 pr-16 border border-gray-300 focus:outline-none focus:ring-2"
+          placeholder="Entrez du texte"
         />
         <button
           type="submit"
-          class="px-4 py-2 rounded bg-yellow-500 text-black font-semibold hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+          class="absolute inset-y-0 right-0 px-4 py-2 m-2 bg-yellow-500 text-white hover: focus:outline-none"
         >
           Subscribe
         </button>
-      </form>
+      </div>
     </div>
+    <hr class="border border-gray-200" />
   </div>
 </template>
 
 <script setup>
-// Define a data property for the email
-import { ref } from "vue";
-const email = ref("");
-
-// Define a submit handler for the form
-const handleSubscribe = () => {
-  alert(`Subscribed with ${email.value}`);
-};
 </script>
 
 <style>
 .bg-pattern {
-  background-image: url("public/img/subBg.png");
+  background-image: url("/img/subBg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
